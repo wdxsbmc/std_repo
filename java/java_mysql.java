189a1911b452dc057c04e2e3a4a0fa05c48d28ac
milo.li <wdxsbmc@gmail.com>
2018-05-18 10:10:17 +0800

    jdbc connect ok

--- java/java_mysql.java
+++ java/java_mysql.java
@@ -4,16 +4,33 @@ public class java_mysql
 {
 	public static void main(String arg[])
 	{
+		
 		try
 		{
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection ct = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/mydata?user=root&passwd=12345&useSSL=true");
+			String user ="root";
+			String password = "12345";
+			String url = "jdbc:mysql://localhost:3306/mydata?useSSL=false";
+			Connection ct = DriverManager.getConnection(url,user,password);
 			Statement sm = ct.createStatement();
 			ResultSet rs = sm.executeQuery("Select * from user_info");
 			while(rs.next())
 			{
 				System.out.println(rs.getString("info_name"));
 			}
+			/*
+			String url="jdbc:mysql://127.0.0.1:3306/mydata?useSSL=false";
+	 		String username="root";
+	 		String password="12345";
+	 		Connection ct = DriverManager.getConnection(url, username, password);
+	 		Statement sm = ct.createStatement();
+	 		ResultSet rs = sm.executeQuery("Select * from user_info");
+			*/
+			while(rs.next())
+			{
+				System.out.println(rs.getString("info_name"));
+			}
+
 		}
 		catch(Exception e)
 		{
