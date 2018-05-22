0a16a01fae955fa31b4253df9990d33521db601d
Milo.Li <milo.li@localhost.localdomain>
2018-05-22 17:18:46 +0800

    udpate mysql

--- mysql/mysql.sql
+++ mysql/mysql.sql
@@ -36,3 +36,36 @@ select * from user_info order by 'info_name';
 
 #show  database engines;
 show engines;
+
+
+#transaction
+show variables like '%commit%';
+
+start transaction;
+select * from user_info;
+update  user_info set  info_name='caca' where info_name='aaa';
+update  user_info set  info_name='cbcb' where info_name='bbb';
+select * from user_info;
+rollback;
+select * from user_info;
+
+update  user_info set  info_name='caca' where info_name='aaa';
+update  user_info set  info_name='cbcb' where info_name='bbb';
+commit;
+select * from user_info;
+
+#create index
+create index  index_user_info on  user_info(info_name);
+
+show  index from user_info;
+
+drop  index index_user_info on  user_info;
+
+
+#view
+create view  view_user_info as select * from user_info;
+select  * from view_user_info;
+drop view if exists view_user_info;
+
+
+
